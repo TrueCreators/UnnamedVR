117e866e3e3f13709dcb423745ad9755464c944a
YouName <truecreators@gmail.com>
2022-02-16 00:35:18 +0300

    Added VR C++ Player with ALSv4(needs merge MovementComponent)

---
+++ Source/UnnamedVR/Player/VRPlayer.cpp
@@ -0,0 +1,113 @@
+﻿
+#include "VRPlayer.h"
+
+#include "Engine/StaticMesh.h"
+#include "AI/ALSAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+AVRPlayer::AVRPlayer(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	HeldObjectRoot = CreateDefaultSubobject<USceneComponent>(TEXT("HeldObjectRoot"));
+	HeldObjectRoot->SetupAttachment(GetMesh());
+
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMesh->SetupAttachment(HeldObjectRoot);
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(HeldObjectRoot);
+
+	AIControllerClass = AALSAIController::StaticClass();
+}
+
+void AVRPlayer::ClearHeldObject()
+{
+	StaticMesh->SetStaticMesh(nullptr);
+	SkeletalMesh->SetSkeletalMesh(nullptr);
+	SkeletalMesh->SetAnimInstanceClass(nullptr);
+}
+
+void AVRPlayer::AttachToHand(UStaticMesh* NewStaticMesh, USkeletalMesh* NewSkeletalMesh, UClass* NewAnimClass,
+                                 bool bLeftHand, FVector Offset)
+{
+	ClearHeldObject();
+
+	if (IsValid(NewStaticMesh))
+	{
+		StaticMesh->SetStaticMesh(NewStaticMesh);
+	}
+	else if (IsValid(NewSkeletalMesh))
+	{
+		SkeletalMesh->SetSkeletalMesh(NewSkeletalMesh);
+		if (IsValid(NewAnimClass))
+		{
+			SkeletalMesh->SetAnimInstanceClass(NewAnimClass);
+		}
+	}
+
+	FName AttachBone;
+	if (bLeftHand)
+	{
+		AttachBone = TEXT("VB LHS_ik_hand_gun");
+	}
+	else
+	{
+		AttachBone = TEXT("VB RHS_ik_hand_gun");
+	}
+
+	HeldObjectRoot->AttachToComponent(GetMesh(),
+	                                  FAttachmentTransformRules::SnapToTargetNotIncludingScale, AttachBone);
+	HeldObjectRoot->SetRelativeLocation(Offset);
+}
+
+void AVRPlayer::RagdollStart()
+{
+	ClearHeldObject();
+	Super::RagdollStart();
+}
+
+void AVRPlayer::RagdollEnd()
+{
+	Super::RagdollEnd();
+	UpdateHeldObject();
+}
+
+ECollisionChannel AVRPlayer::GetThirdPersonTraceParams(FVector& TraceOrigin, float& TraceRadius)
+{
+	const FName CameraSocketName = bRightShoulder ? TEXT("TP_CameraTrace_R") : TEXT("TP_CameraTrace_L");
+	TraceOrigin = GetMesh()->GetSocketLocation(CameraSocketName);
+	TraceRadius = 15.0f;
+	return ECC_Camera;
+}
+
+FTransform AVRPlayer::GetThirdPersonPivotTarget()
+{
+	return FTransform(GetActorRotation(),
+	                  (GetMesh()->GetSocketLocation(TEXT("Head")) + GetMesh()->GetSocketLocation(TEXT("root"))) / 2.0f,
+	                  FVector::OneVector);
+}
+
+FVector AVRPlayer::GetFirstPersonCameraTarget()
+{
+	return GetMesh()->GetSocketLocation(TEXT("FP_Camera"));
+}
+
+void AVRPlayer::OnOverlayStateChanged(EALSOverlayState PreviousState)
+{
+	Super::OnOverlayStateChanged(PreviousState);
+	UpdateHeldObject();
+}
+
+void AVRPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateHeldObjectAnimations();
+}
+
+void AVRPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UpdateHeldObject();
+}
---
+++ Source/UnnamedVR/Player/VRPlayer.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include "VRBasePlayer.h"
+#include "VRPlayer.generated.h"
+
+UCLASS()
+class UNNAMEDVR_API AVRPlayer : public AVRBasePlayer
+{
+	GENERATED_BODY()
+
+public:
+	AVRPlayer(const FObjectInitializer& ObjectInitializer);
+
+	/** Implemented on BP to update held objects */
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "ALS|HeldObject")
+	void UpdateHeldObject();
+
+	UFUNCTION(BlueprintCallable, Category = "ALS|HeldObject")
+	void ClearHeldObject();
+
+	UFUNCTION(BlueprintCallable, Category = "ALS|HeldObject")
+	void AttachToHand(UStaticMesh* NewStaticMesh, USkeletalMesh* NewSkeletalMesh,
+					  class UClass* NewAnimClass, bool bLeftHand, FVector Offset);
+
+	virtual void RagdollStart() override;
+
+	virtual void RagdollEnd() override;
+
+	virtual ECollisionChannel GetThirdPersonTraceParams(FVector& TraceOrigin, float& TraceRadius) override;
+
+	virtual FTransform GetThirdPersonPivotTarget() override;
+
+	virtual FVector GetFirstPersonCameraTarget() override;
+
+protected:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+	virtual void OnOverlayStateChanged(EALSOverlayState PreviousState) override;
+
+	/** Implement on BP to update animation states of held objects */
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "ALS|HeldObject")
+	void UpdateHeldObjectAnimations();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
+	USceneComponent* HeldObjectRoot = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
+	USkeletalMeshComponent* SkeletalMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
+	UStaticMeshComponent* StaticMesh = nullptr;
+
+private:
+	bool bNeedsColorReset = false;
+	
+};
